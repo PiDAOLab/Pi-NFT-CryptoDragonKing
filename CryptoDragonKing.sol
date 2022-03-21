d8168f634e6d357ba4881bdc219f948125012dde
M君 <83874614+HashPiM@users.noreply.github.com>
2022-03-21 13:46:36 +0800

    Update CryptoDragonKing.sol
    智能合约部分

--- CryptoDragonKing.sol
+++ CryptoDragonKing.sol
@@ -6,9 +6,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
+import "./ERC721Enumerable.sol";
+import "./Ownable.sol";
+import "./Strings.sol";
 
 contract CryptoDragonKing is ERC721Enumerable, Ownable {
     using Strings for uint256;
